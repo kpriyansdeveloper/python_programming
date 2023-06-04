0f49e5a90cbe8163cf5b1254b9fc33baa7bd5824
Priyanshu Kumar <priyanshuofficework@gmail.com>
2023-06-04 14:22:42 +0530

    Day 2 Exercise

---
+++ Exercise_2/Data_Types.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+b = two_digit_number[0]
+c = two_digit_number[1]
+print(int(b)+ int(c))
--- Input Function.py
+++
@@ -1,4 +0,0 @@
-#Write your code below this line 👇
-
-#name = input()
-print( len( input( "What is your name? ") ) )
